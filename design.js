8d7df39342fc4f166b18990d7e7b48151e7cccd6
AthInamdar <151920982+AthInamdar@users.noreply.github.com>
2024-03-21 00:38:11 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,13 @@
+
+
+let typed = new Typed(".text", {
+    strings: [" Web Designer"," Frontend Developer","Django Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backdelay: 1000,
+    loop: true
+});
+
+document.getElementById('update').addEventListener('click', function() {
+    alert('It will updated soon...');
+})
